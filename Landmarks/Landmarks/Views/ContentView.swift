95b1022106492ecd463c90c87a66151314293f04
Aliaksei Mastounikau <amastoun@its.jnj.com>
2023-04-11 17:40:30 +0200

    Complete 'Building Lists and Navigation' tutorial

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Aliaksei Mastounikau on 9.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarksList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/Views/LandmarksList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Aliaksei Mastounikau on 11.04.23.
+//
+
+import SwiftUI
+
+struct LandmarksList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)", "iPhone 14 Pro", "iPad Air (5th generation)"], id: \.self) { deviceName in
+            LandmarksList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
