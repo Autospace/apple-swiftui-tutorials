7fdbbce166160f4197b7751a2681e17a60d36c26
MALX <mostovnikov.alex@gmail.com>
2023-04-29 19:19:08 +0200

    Complete section 5 of 'Creating of macOS app' tutorial

---
+++ Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Aliaksei Mastounikau on 29.04.23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
